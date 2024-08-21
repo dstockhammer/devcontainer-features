3f597b517fa4901573241e82a44a817d69be174a
Dan <daniel@stockhammer.it>
2024-08-21 16:51:29 +0200

    handle git clone error

--- src/zsh/install.sh
+++ src/zsh/install.sh
@@ -13,8 +13,11 @@ if ! type git > /dev/null 2>&1; then
   exit 1
 fi
 
-echo "Activating feature 'dstockhammer/zsh'"
-
-git clone --recurse-submodules https://github.com/dstockhammer/zsh.git $_REMOTE_USER_HOME/zsh
-echo 'source $HOME/zsh/.zshrc' > $_REMOTE_USER_HOME/.zshrc
-sudo chsh -s $(which zsh) $_REMOTE_USER
+if git clone --recurse-submodules https://github.com/dstockhammer/zsh.git $_REMOTE_USER_HOME/zsh; then
+  echo "Activating feature 'dstockhammer/zsh'"
+  echo 'source $HOME/zsh/.zshrc' > $_REMOTE_USER_HOME/.zshrc
+  sudo chsh -s $(which zsh) $_REMOTE_USER
+else
+  echo "Failed to activate feature 'dstockhammer/zsh':"
+  echo "Unable to clone https://github.com/dstockhammer/zsh.git"
+fi
