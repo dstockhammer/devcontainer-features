dc5f8b64fcadd2dbd2c8d44b88d084a700670c5e
Dan <daniel@stockhammer.it>
2024-08-21 16:16:33 +0200

    add zsh feature

---
+++ src/zsh/README.md
@@ -0,0 +1,13 @@
+# Dan's oh-my-zsh config
+
+Sets up zsh as default shell with [Dan's oh-my-zsh config](https://github.com/dstockhammer/zsh).
+
+## Example Usage
+
+```json
+"features": {
+    "ghcr.io/devcontainers/feature-starter/hello:1": {
+        "version": "latest"
+    }
+}
+```
---
+++ src/zsh/install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+
+if ! type zsh > /dev/null 2>&1; then
+  echo "Failed to activate feature 'dstockhammer/zsh':"
+  echo "Unable to find zsh. Please make sure it is installed."
+  exit 1
+fi
+
+if ! type git > /dev/null 2>&1; then
+  echo "Failed to activate feature 'dstockhammer/zsh':"
+  echo "Unable to find git. Please make sure it is installed."
+  exit 1
+fi
+
+echo "Activating feature 'dstockhammer/zsh'"
+
+git clone --recurse-submodules https://github.com/dstockhammer/zsh.git $_REMOTE_USER_HOME/zsh
+echo 'source $HOME/zsh/.zshrc' > $_REMOTE_USER_HOME/.zshrc
+sudo chsh -s $(which zsh) $_REMOTE_USER
